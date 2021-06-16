f3cb7593cbb766ae069d1991fa9f39f51210b73d
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-16 16:32:08 +0900

    [Feature] - Square array price

---
+++ src/Array/IndexSquare.java
@@ -0,0 +1,18 @@
+package Array;
+
+import static java.lang.System.out;
+
+public class IndexSquare {
+    public static void main(String[] args) {
+        int[] num1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int[] num2 = new int[10];
+
+        for (int i = 0; i < num1.length; i++) {
+            num2[i] += num1[i] * num1[i];
+        }
+
+        for (int i = 0; i < num2.length; i++) {
+            out.println(num2[i]);
+        }
+    }
+}
