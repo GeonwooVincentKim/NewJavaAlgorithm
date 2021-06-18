1d79dd0adfaeea40da5fa06bdf1fd0df7b6f03d2
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-18 14:05:30 +0900

    [Feature] - Founded the answer why it shows error

--- src/Class/Inheritance/InheritanceExample.java
+++ src/Class/Inheritance/InheritanceExample.java
@@ -9,9 +9,29 @@ class Parent {
         out.println();
     }
 
+    // Could be run the risk that in the lines above this constructor's referencing
+    // a state in the superclass that's not initialized yet.
+
+    // And the compiler cannot be sure of which `this` method it will going to
+    // execute
+    // So it occurs error `Constructor call must be the first statement in a
+    // constructor`.
+
+    // Parent() {
+    // this(1);
+    // this("String");
+    // out.print("(1) -> Parent Class Initializer without parameter");
+    // out.println();
+    // }
+
     Parent(int intParameter) {
-        out.print("(2) -> Parent Class Initializer with Parameter");
+        this("String");
+        out.print("(2) -> Parent Class Initializer with Integer Parameter");
         out.println();
+    }
+
+    Parent(String stringParameter) {
+        out.print("(2-1) -> Parent Class Initializer with String Parameter");
     }
 }
 
