11399e424a72c603aa71f8d39fd7b2e853e4ca41
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-21 10:16:31 +0900

    [Feature] - Convert file as Method

--- src/Class/Inheritance/Annotation/Test.java
+++ src/Class/Inheritance/Annotation/Test.java
@@ -5,65 +5,82 @@ import static java.lang.System.out;
 import java.util.Scanner;
 
 public class Test {
-    public static int[] getPastaCost(int pasta) {
-        Scanner sc = new Scanner(System.in);
+    public static int[] getPastaCost(Scanner sc, int pasta) {
         int[] pastaCost = new int[pasta];
 
         for (int i = 0; i < pasta; i++) {
             pastaCost[i] = sc.nextInt();
         }
 
-        sc.close();
         return pastaCost;
     }
 
-    public static String printPastaCost(int[] pastaArray) {
-        String pastaLoop = "";
+    // public static String printPastaCost(int[] pastaArray) {
+    // String pastaLoop = "";
 
-        for (int i = 0; i < pastaArray.length; i++) {
-            pastaLoop += pastaArray[i] + " ";
+    // for (int i = 0; i < pastaArray.length; i++) {
+    // pastaLoop += pastaArray[i] + " ";
+    // }
+
+    // return pastaLoop;
+    // }
+
+    public static int[] getJuiceCost(Scanner sc, int juice) {
+        int[] juiceCost = new int[juice];
+
+        for (int i = 0; i < juice; i++) {
+            juiceCost[i] = sc.nextInt();
         }
 
-        return pastaLoop;
+        return juiceCost;
+    }
+
+    // public static String printJuiceCost(int[] juiceArray) {
+    // String juiceLoop = "";
+
+    // for (int i = 0; i < juiceArray.length; i++) {
+    // juiceLoop += juiceArray[i] + " ";
+    // }
+
+    // return juiceLoop;
+    // }
+
+    public static double getMinimumCost(int[] pastaCost, int[] juiceCost, double min) {
+        int sum = 0;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2; j++) {
+                sum = pastaCost[i] + juiceCost[j];
+
+                if (min > sum) {
+                    min = sum;
+                }
+            }
+        }
+
+        return min;
+    }
+
+    public static double discountMinimumPrice(double min) {
+        min = (double) (min * 1.1);
+        return min;
     }
 
     public static void main(String[] args) {
-        // Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        // int[] pastaCost = new int[3];
         int pasta = 3;
-        // int juice = 2;
+        int juice = 2;
 
-        int[] pastaCost = getPastaCost(pasta);
-        String getResult = printPastaCost(pastaCost);
-        out.println(getResult);
-        // int[] juiceCost = new int[2];
+        int[] pastaCost = getPastaCost(sc, pasta);
+        int[] juiceCost = getJuiceCost(sc, juice);
 
-        // int sum = 0;
-        // double min = 10000;
+        double min = 10000;
+        min = getMinimumCost(pastaCost, juiceCost, min);
+        min = discountMinimumPrice(min);
 
-        // for (int i = 0; i < pastaCost.length; i++) {
-        // pastaCost[i] = sc.nextInt();
-        // }
+        out.println(String.format("%.1f", min));
 
-        // for (int i = 0; i < juiceCost.length; i++) {
-        // juiceCost[i] = sc.nextInt();
-        // }
-
-        // for (int i = 0; i < 3; i++) {
-        // for (int j = 0; j < 2; j++) {
-        // sum = pastaCost[i] + juiceCost[j];
-
-        // if (min > sum) {
-        // min = sum;
-        // }
-        // }
-        // }
-
-        // min = (double) (min * 1.1);
-
-        // out.println(String.format("%.1f", min));
-
-        // sc.close();
+        sc.close();
     }
 }
