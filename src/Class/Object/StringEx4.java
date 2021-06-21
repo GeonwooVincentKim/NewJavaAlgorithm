9cf504ab4ceaa47b41610a921c0866d99c5d6d23
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-21 15:19:35 +0900

    [Feature] - Successfully convert Char[] to String[]

--- src/Class/Object/StringEx4.java
+++ src/Class/Object/StringEx4.java
@@ -5,6 +5,7 @@ import static java.lang.System.out;
 public class StringEx4 {
     public static void main(String[] args) {
         String text = "Hello My Name is Hong Gil Dong";
+        String charText = "GoodBye, Hong Gil Dong";
         out.println(text);
 
         out.println("0번 Index : " + text.charAt(0));
@@ -13,20 +14,35 @@ public class StringEx4 {
             out.print(text.charAt(i) + " ");
         }
 
+        out.println();
+        out.println();
+        out.println("-----------------------------------");
+        out.println();
+
         String[] stringArray = text.split(" ");
         for (int i = 0; i < stringArray.length; i++) {
             out.println(i + " 번 Index 값 = " + stringArray[i]);
         }
 
         out.println();
+        out.println("-----------------------------------");
+        out.println();
 
-        char[] charArray = new char[text.length()];
+        // char[] charArray = new char[text.length()];
         // char[] charArray = text.toCharArray();
+        char[] charArray = charText.toCharArray();
+        String getCharArray = String.valueOf(charArray);
 
-        for (int i = 0; i < charArray.length; i++) {
-            charArray[i] = text.charAt(i);
-            out.println(i + " 번 Index (char[]) 의 값 = " + charArray[i]);
+        String[] convertStringArray = getCharArray.split(" ");
+
+        for (int i = 0; i < convertStringArray.length; i++) {
+            out.println(i + " 번 Index 값 (Char[] -> String[]) = " + convertStringArray[i]);
         }
 
+        out.println();
+        // for (int i = 0; i < charArray.length; i++) {
+        // // charArray[i] = text.charAt(i);
+        // out.println(i + " 번 Index (char[]) 의 값 = " + charArray[i]);
+        // }
     }
 }
