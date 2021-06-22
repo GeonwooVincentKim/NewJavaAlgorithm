07717b1205784528c6038625e4902720ceb8995c
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-22 17:00:03 +0900

    [Modify] - Trying to implement add method and get method

--- src/Collections/List/ArrayList/ArrayListExample.java
+++ src/Collections/List/ArrayList/ArrayListExample.java
@@ -2,6 +2,9 @@ package Collections.List.ArrayList;
 
 import static java.lang.System.out;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class ArrayListExample {
     public static void main(String[] args) {
 
@@ -16,6 +19,7 @@ public class ArrayListExample {
 
         for (int i = 0; i < customArrayList.size(); i++) {
             out.println(customArrayList.get(i));
+            out.println(customArrayList.getIndex());
         }
     }
 }
--- src/Collections/List/ArrayList/CustomArrayList.java
+++ src/Collections/List/ArrayList/CustomArrayList.java
@@ -3,20 +3,71 @@ package Collections.List.ArrayList;
 import static java.lang.System.out;
 
 public class CustomArrayList {
-    // public static final int DEFAULT_SIZE = 10;
-    private int size = 10;
+    private Object[] array = new Object[100];
     private int index;
-    private Object[] elementData = new Object[100];
+    private int size = 10;
+    private boolean isEmpty;
+    private static final int DEFAULT_SIZE = 10;
 
-    CustomArrayList() {
+    public CustomArrayList() {
+        this.size = DEFAULT_SIZE;
     }
 
-    public boolean addFirst(Object element) {
+    public CustomArrayList(int arrayLength) {
+        this.setArray(array);
+        this.setSize(size);
+        this.setIndex(index);
+    }
+
+    public static int getDefaultSize() {
+        return DEFAULT_SIZE;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public Object[] getarray() {
+        return array;
+    }
+
+    public void setArray(Object[] array) {
+        this.array = array;
+    }
+
+    // When the Index elemenet is the FirstIndex of the Array
+    public boolean isFirstIndex(Object element) {
         return add(0, element);
     }
 
+    // When the Index element is not lastIndex, but trying to insert between element
+    // and elements
+    public boolean add(int index, Object element) {
+        for (int i = size; i >= index; i--) {
+            array[i + 1] = array[i];
+        }
+
+        array[index] = element;
+        size++;
+
+        return true;
+    }
+
+    // When the Index element is lastIndex of the Array
     public boolean add(Object element) {
-        elementData[size] = element;
+        array[index] = element;
         size++;
 
         return true;
@@ -26,27 +77,28 @@ public class CustomArrayList {
         if (i > this.index) {
             out.println("Invalid");
             return null;
-        } else if (this.index == this.elementData.length) {
-            out.println("Array is Empty");
+        } else if (this.isEmpty) {
             return null;
         } else {
-            return this.elementData[i];
+            return this.array[i];
         }
-
-    }
-
-    public boolean add(int index, Object element) {
-        for (int i = size; i >= index; i--) {
-            elementData[i + 1] = elementData[i];
-        }
-
-        elementData[index] = element;
-        size++;
-
-        return true;
     }
 
     public int size() {
         return size;
+    }
+
+    public boolean isEmpty() {
+        return (this.index == 0);
+    }
+
+    public Object remove(int i) {
+        Object returnO = this.array[i];
+        for (int j = i; j < this.index - 1; j++) {
+            this.array[j] = this.array[j + 1];
+        }
+
+        this.index--;
+        return returnO;
     }
 }
