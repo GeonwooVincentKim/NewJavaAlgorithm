15d389e0a09d95805c4a56f657006d78e4adae3b
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-22 10:19:46 +0900

    [Feature] - Regax for checking `bat` exist

--- src/Class/Object/Regex/RegexExample.java
+++ src/Class/Object/Regex/RegexExample.java
@@ -7,10 +7,22 @@ import java.util.regex.Pattern;
 
 public class RegexExample {
     public static void main(String[] args) {
+        // Rule for that the lower-case of alphabet starting from `b` and has over 0
         Pattern p = Pattern.compile("b[a-z]*");
         out.println(p);
 
-        Matcher m = p.matcher("bat");
+        Pattern p1 = Pattern.compile("c[a-z]*");
+        out.println(p1);
+
+        // Check Character-Sequence(String), "bat"
+        Matcher m;
+        m = p.matcher("bat");
         out.println(m);
+        out.println("bat = " + m.matches());
+
+        // Check Character-Sequence(String), "cat"
+        m = p.matcher("cat");
+        out.println(m);
+        out.println("cat = " + m.matches());
     }
 }
