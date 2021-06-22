6acb93494eb9eb362c47452337825c9d335de4ce
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-22 10:57:02 +0900

    [Feature] - Regular Expression for checking the data is valid

--- src/Class/Object/Regex/RegaxExample2.java
+++ src/Class/Object/Regex/RegaxExample2.java
@@ -4,7 +4,11 @@ import static java.lang.System.out;
 
 public class RegaxExample2 {
     public static void main(String[] args) {
-        String[] patterns = { ".", "[a-z]?" };
+        String[] patterns = { ".", "[a-z]?", "[0-9]+", "0[1-9]*", "^[0-9]", "[^0-9]", "[a-z]*", "[a-z]+", "02|010",
+                "\\s", "\\S", "\\d", "\\w", "\\W" };
         out.println(patterns);
+
+        String[] datas = { "bat", "021231234", "12345", "011", "bed", "02", "A", "9", "a", "*" };
+        out.println(datas);
     }
 }
