5f78d819f5705a4539ca6cd6ebddc0f4d38db712
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-22 09:26:02 +0900

    [Feature] - Took 0 second to get all of String

---
+++ src/Class/Object/RealStringBuffer.java
@@ -0,0 +1,20 @@
+package Class.Object;
+
+import static java.lang.System.out;
+
+public class RealStringBuffer {
+    public static void main(String[] args) {
+        out.println();
+
+        long start = System.currentTimeMillis();
+        out.print(start);
+
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < 1000000; i++) {
+            sb.append(i);
+        }
+
+        long end = System.currentTimeMillis();
+        out.println("실행 시간 : " + (end - start) / 1000);
+    }
+}
