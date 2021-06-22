2efdba15094971a83ce80ef6baaab9c8be48e347
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-22 11:18:18 +0900

    [Feature] - Find out the same pattern

--- src/Class/Object/Regex/RegaxExample2.java
+++ src/Class/Object/Regex/RegaxExample2.java
@@ -2,6 +2,9 @@ package Class.Object.Regex;
 
 import static java.lang.System.out;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegaxExample2 {
     public static void main(String[] args) {
         String[] patterns = { ".", "[a-z]?", "[0-9]+", "0[1-9]*", "^[0-9]", "[^0-9]", "[a-z]*", "[a-z]+", "02|010",
@@ -10,5 +13,20 @@ public class RegaxExample2 {
 
         String[] datas = { "bat", "021231234", "12345", "011", "bed", "02", "A", "9", "a", "*" };
         out.println(datas);
+
+        // for (String d : datas) {
+        for (int i = 0; i < datas.length; i++) {
+            out.println(datas[i] + " -> 문자와 일치하는 패턴 : ");
+
+            for (String p : patterns) {
+                Pattern pattern = Pattern.compile(p);
+                Matcher m = pattern.matcher(datas[i]);
+
+                if (m.matches()) {
+                    out.print(p + ", ");
+                }
+
+            }
+        }
     }
 }
