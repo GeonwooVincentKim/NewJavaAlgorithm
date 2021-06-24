aeb91c41bcd499c44706738dd3c10bcf381c3f32
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-25 08:57:52 +0900

    [Feature] - Print ForLoop and Iterator

--- src/Stream/IteratorExample.java
+++ src/Stream/IteratorExample.java
@@ -1,6 +1,7 @@
 package Stream;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 // import java.util.Arrays;
 import java.util.List;
 
@@ -12,13 +13,36 @@ public class IteratorExample {
 
         List<String> arrayList = new ArrayList<String>();
 
+        // 1. For-Loop
+        out.println("for 문 이용 -> (Array 사용)");
         for (int i = 0; i < array.length; i++) {
             arrayList.add(array[i]);
             out.print(arrayList.get(i) + " ");
         }
 
+        out.println(arrayList);
+        out.println("-----------------------------");
+
+        // 2. Outer-Iterator
+        out.println("외부 반복자 이용 -> (Array 사용)");
+        Iterator<String> iterator = arrayList.iterator();
+        out.println(iterator);
+
+        while (iterator.hasNext()) {
+            out.println(iterator.next());
+        }
+
+        out.println("-----------------------------");
+
+        // 3. Inner-Iterator
+        out.println("내부 반복자 이용 -> (Array 사용)");
+        arrayList.stream().forEach(s -> out.println(s));
+
+        out.println("=============================");
+
+        // 4. For-Loop
+
         // List<String> list = Arrays.asList(new String[] { "홍길동", "김유신", "이순신", "유관순"
         // });
-        out.println(arrayList);
     }
 }
