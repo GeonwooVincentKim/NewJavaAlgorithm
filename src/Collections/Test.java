1776fd7510d1ceee200b8249da1d6e30bb6ec4ec
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-24 09:29:02 +0900

    [Feature] - Sorting Random-Array

--- src/Collections/Test.java
+++ src/Collections/Test.java
@@ -1,44 +1,76 @@
 package Collections;
 
-import java.util.Arrays;
+// import java.util.Arrays;
+import static java.lang.System.out;
+
+import java.util.Scanner;
 
 public class Test {
-    public static void main(String[] args) {
-        int[] arr = { 1, 4, 8, 12, 18, 20, 23, 26, 30 };
-        Arrays.sort(arr); // 올림차순 정렬
-
-        System.out.println(binarySearch_recursion(arr, 0, arr.length - 1, 4));
-        System.out.println(binarySearch_loop(arr, 0, arr.length - 1, 23));
+    public static int randomRange(int min, int max) {
+        return (int) (Math.random() * (max - min + 1)) + 11;
     }
 
-    public static int binarySearch_recursion(int[] arr, int start, int last, int no) {
-        int mid = (int) (start + last) / 2; // 중간 값(나머지는 버린다)
+    public static int[] setRandomArray(int userInput) {
+        int[] randomArray = new int[userInput];
 
-        if (arr[mid] == no) {
-            return mid;
-        } else if (arr[mid] > no) {
-            // 중간 값이 찾고자 하는 값보다 큰 경우
-            last = mid - 1;
-            return binarySearch_recursion(arr, start, last, no);
-        } else {
-            // 중간 값이 찾고자 하는 값보다 작은 경우
-            start = mid + 1;
-            return binarySearch_recursion(arr, start, last, no);
+        for (int i = 0; i < userInput; i++) {
+            randomArray[i] = randomRange(10, 50);
         }
+
+        return randomArray;
     }
 
-    public static int binarySearch_loop(int[] arr, int start, int last, int no) {
-        while (true) {
-            int mid = (int) (start + last) / 2; // 중간 값(나머지는 버린다)
-            if (arr[mid] == no) {
-                return mid;
-            } else if (arr[mid] > no) {
-                // 중간 값이 찾고자 하는 값보다 큰 경우
-                last = mid - 1;
-            } else {
-                // 중간 값이 찾고자 하는 값보다 작은 경우
-                start = mid + 1;
+    public static String printRandomArray(int[] randomArray) {
+        String loopResult = "";
+
+        for (int i = 0; i < randomArray.length; i++) {
+            loopResult += randomArray[i] + " ";
+        }
+
+        return loopResult;
+    }
+
+    public static int[] sortRandomArray(int[] randomArray) {
+        int[] sortArray = new int[randomArray.length];
+
+        for (int i = 0; i < sortArray.length; i++) {
+            for (int j = i + 1; j < sortArray.length; j++) {
+                if (randomArray[i] > randomArray[j]) {
+                    int temp = randomArray[i];
+                    randomArray[i] = randomArray[j];
+                    randomArray[j] = temp;
+
+                    sortArray = randomArray;
+                }
             }
         }
+
+        return sortArray;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        out.print("입력받으실 숫자의 길이를 입력하세요 : ");
+
+        // 1. Set the array length by user Input-Value.
+        int userInput = sc.nextInt();
+
+        // 2. Get the random-number as the number entered by User.
+        int[] randomArray = setRandomArray(userInput);
+        String printArray = printRandomArray(randomArray);
+        out.println(printArray);
+
+        // 3. Sort array before find the value.
+        int[] sortArray = sortRandomArray(randomArray);
+        // out.println(sortArray);
+        printArray = printRandomArray(sortArray);
+        out.println(printArray);
+
+        // 4. Find the index using `Binary-Search`.
+        // In this case, you can use `While()` Loop or `Recursive-Function()`
+        // 5. Get the result where the index located and display to User
+        // that can add the new index between another indexs or not.
+
+        sc.close();
     }
 }
