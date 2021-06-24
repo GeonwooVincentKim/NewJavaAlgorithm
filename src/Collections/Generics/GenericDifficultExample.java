dab971a436bbb0e85fd888d01f7b9c014478a02b
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-24 14:01:59 +0900

    [Feature] - Get Gen 1 and Gen3 Type

--- src/Collections/Generics/GenericDifficultExample.java
+++ src/Collections/Generics/GenericDifficultExample.java
@@ -4,8 +4,37 @@ import static java.lang.System.out;
 
 public class GenericDifficultExample {
     public static void main(String[] args) {
+        out.println("Gen3 Type -> ");
+
         Gen3 g3 = new Gen3();
-        out.println(g3);
+        out.println(g3.getClass());
+
+        out.println("---------------------------");
+        out.println("Reference ParentClass Data-Type (Gen3 => Gen1(Gen2)) -> ");
+
+        Generic5<? super Gen2> g2 = new Generic5<Gen1>(g3);
+        out.println(g2.getClass());
+
+        out.println("---------------------------");
+        out.println("Autonomous Crossover (Gen3 => Gen1) -> ");
+
+        Gen1 t2 = (Gen3) g2.get();
+        out.println(t2.getClass());
+
+        out.println("---------------------------");
+        out.println("Object Type -> ");
+
+        Object t1 = g2.get();
+        out.println(t1.getClass());
+
+        out.println("---------------------------");
+        out.println("Get t2 Type -> ");
+
+        out.println(t2.name);
+        out.println(t2.getName());
+
+        out.println("---------------------------");
+        out.println("Get t2 Type -> ");
     }
 }
 
