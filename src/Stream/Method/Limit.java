4b6a7e4802fa136c413738a6beaff6f151889844
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-25 09:43:34 +0900

    [Feature] - Stream with Infinite-Loop

---
+++ src/Stream/Method/Limit.java
@@ -0,0 +1,14 @@
+package Stream.Method;
+
+import static java.lang.System.out;
+
+import java.util.stream.Stream;
+
+public class Limit {
+    public static void main(String[] args) {
+        Stream<String> stream = Stream.generate(() -> "애국가");
+        out.println(stream);
+
+        stream.forEach(s -> out.println(s));
+    }
+}
