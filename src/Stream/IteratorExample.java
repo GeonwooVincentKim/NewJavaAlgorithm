6112662e265fb7a7188222f6a00d4bf9139dca25
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-25 09:09:08 +0900

    [Feature] - Iterator for only List

--- src/Stream/IteratorExample.java
+++ src/Stream/IteratorExample.java
@@ -2,7 +2,7 @@ package Stream;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-// import java.util.Arrays;
+import java.util.Arrays;
 import java.util.List;
 
 import static java.lang.System.out;
@@ -38,11 +38,35 @@ public class IteratorExample {
         out.println("내부 반복자 이용 -> (Array 사용)");
         arrayList.stream().forEach(s -> out.println(s));
 
+        out.println();
         out.println("=============================");
+        out.println();
+
+        List<String> list = Arrays.asList(new String[] { "홍길동", "김유신", "이순신", "유관순" });
+        out.println(list);
 
         // 4. For-Loop
+        out.println("for 문 이용");
 
-        // List<String> list = Arrays.asList(new String[] { "홍길동", "김유신", "이순신", "유관순"
-        // });
+        for (int i = 0; i < list.size(); i++) {
+            out.println(list.get(i));
+        }
+
+        out.println("-----------------------------");
+
+        // 5. Outer-Iterator
+        out.println("외부 반복자 이용");
+        Iterator<String> iterator2 = list.iterator();
+        out.println(iterator2);
+
+        while (iterator2.hasNext()) {
+            out.println(iterator2.next());
+        }
+
+        out.println("-----------------------------");
+
+        // 6. Inner-Iterator
+        out.println("내부 반복자 이용");
+        list.stream().forEach(s -> out.println(s));
     }
 }
