c0243d45bf93a3d26020f4a03fa4e7044d138405
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-28 08:45:09 +0900

    [Feature] - Implemented BuffereReader and PrintWriter

--- src/Network/TCP/TCPExample.java
+++ src/Network/TCP/TCPExample.java
@@ -2,7 +2,10 @@ package Network.TCP;
 
 import static java.lang.System.out;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -20,6 +23,9 @@ public class TCPExample {
                 out.println("Waiting for Client access to Server");
                 Socket client = server.accept();
                 out.println(client);
+
+                HttpThread ht = new HttpThread(client);
+                ht.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -32,5 +38,32 @@ public class TCPExample {
                 e.printStackTrace();
             }
         }
+    }
+}
+
+class HttpThread extends Thread {
+    private Socket client;
+    BufferedReader bufferedReader;
+    PrintWriter printWriter;
+
+    HttpThread(Socket client) {
+        this.setClient(client);
+
+        try {
+            bufferedReader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            printWriter = new PrintWriter(client.getOutputStream());
+        } catch (IOException e) {
+            out.println(e.getMessage());
+            out.println("------------------");
+            e.printStackTrace();
+        }
+    }
+
+    public Socket getClient() {
+        return client;
+    }
+
+    public void setClient(Socket client) {
+        this.client = client;
     }
 }
