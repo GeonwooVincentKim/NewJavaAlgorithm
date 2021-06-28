03800457e1da05951e0e734ae015e5056904cfe1
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-28 13:23:34 +0900

    [Feature] - Get the MemberVO

--- src/Collections/DataBase/MemberMain.java
+++ src/Collections/DataBase/MemberMain.java
@@ -6,5 +6,9 @@ public class MemberMain {
     public static void main(String[] args) {
         MemberVO vo = new MemberVO();
         out.println(vo);
+
+        vo.setMember_no(2);
+        vo.setId("hong");
+        vo.setName("홍길동");
     }
 }
