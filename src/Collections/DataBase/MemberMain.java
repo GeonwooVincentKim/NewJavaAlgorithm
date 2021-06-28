b5a7e4b4504ac86e52d77a4ef81a09ff282c5f04
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-28 13:20:56 +0900

    [Feature] - Create instance of MemberVO

---
+++ src/Collections/DataBase/MemberMain.java
@@ -0,0 +1,10 @@
+package Collections.DataBase;
+
+import static java.lang.System.out;
+
+public class MemberMain {
+    public static void main(String[] args) {
+        MemberVO vo = new MemberVO();
+        out.println(vo);
+    }
+}
