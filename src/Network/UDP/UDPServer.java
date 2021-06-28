c243688caa629a7712ff6e669d0f381dbc1278ec
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-28 09:46:22 +0900

    [Feature] - Print how does UDPServer works

--- src/Network/UDP/UDPServer.java
+++ src/Network/UDP/UDPServer.java
@@ -2,7 +2,7 @@ package Network.UDP;
 
 import static java.lang.System.out;
 
-// import java.net.DatagramPacket;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
 public class UDPServer {
@@ -11,7 +11,18 @@ public class UDPServer {
             DatagramSocket socket = new DatagramSocket(9500);
             out.println(socket);
 
-            // DatagramPacket inPacket;
+            DatagramPacket inPacket = null;
+            byte[] inMessage = null;
+            out.println(inPacket + "\n" + inMessage);
+
+            while (true) {
+                out.println("--------------------------");
+                inMessage = new byte[1024];
+                inPacket = new DatagramPacket(inMessage, inMessage.length);
+                out.println("InMessage : " + inMessage + "\n" + "inPacket : " + inPacket);
+
+                break;
+            }
         } catch (Exception e) {
             out.println(e.getMessage());
             out.println("<--------------------------->");
