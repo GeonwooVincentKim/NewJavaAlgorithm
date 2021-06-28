25d8bd46c9bedf52d58bc3df4b92ae744f73e83f
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-28 09:54:01 +0900

    [Feature] - Receive Message from Client site

--- src/Network/UDP/UDPServer.java
+++ src/Network/UDP/UDPServer.java
@@ -4,6 +4,7 @@ import static java.lang.System.out;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class UDPServer {
     public static void main(String[] args) {
@@ -21,7 +22,18 @@ public class UDPServer {
                 inPacket = new DatagramPacket(inMessage, inMessage.length);
                 out.println("InMessage : " + inMessage + "\n" + "inPacket : " + inPacket);
 
-                break;
+                // Receive PacketData
+                socket.receive(inPacket);
+
+                // Save as Character-String(String)
+                String message = new String(inMessage, 0, inPacket.getLength());
+                out.println("Client Message : " + message);
+
+                InetAddress address = inPacket.getAddress();
+                int port = inPacket.getPort();
+
+                out.println("Client Address : " + address);
+                out.println("Client Port-Number : " + port);
             }
         } catch (Exception e) {
             out.println(e.getMessage());
