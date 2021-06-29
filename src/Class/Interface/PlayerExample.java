7849edbca9a1f85932ba49e12ba006f0096d8caf
GeonwooVincentKim <kdsnop@gmail.com>
2021-06-29 15:43:09 +0900

    [Feature] - Convert as Anonymous Class

--- src/Class/Interface/PlayerExample.java
+++ src/Class/Interface/PlayerExample.java
@@ -1,9 +1,25 @@
 package Class.Interface;
 
+import static java.lang.System.out;
+
 public class PlayerExample {
     public static void main(String[] args) {
-        Player p1 = new BaseBallPlayer();
-        Player p2 = new FootBallPlayer();
+        // Player p1 = new BaseBallPlayer();
+        // Player p2 = new FootBallPlayer();
+
+        Player p1 = new Player() {
+            @Override
+            public void play() {
+                out.println("야구선수가 야구를 합니다.");
+            }
+        };
+
+        Player p2 = new Player() {
+            @Override
+            public void play() {
+                out.println("축구선수가 축구를 합니다.");
+            }
+        };
 
         playGame(p1);
         playGame(p2);
