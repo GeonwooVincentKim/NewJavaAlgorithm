3fc9a7197528a9170efc10fbda89ddf832264e66
GeonwooVincentKim <kdsnop@gmail.com>
2021-07-06 16:20:35 +0900

    [Update] - Updated filename

--- src/File/FileReader/BufferedReaderExample.java
+++ src/File/FileReader/BufferedReaderExample.java
@@ -6,7 +6,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class FileReaderExample {
+public class BufferedReaderExample {
     public static void main(String[] args) {
         FileReader fr = null;
         BufferedReader br = null;
