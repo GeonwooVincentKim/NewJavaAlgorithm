8f27c4fad057f963c19aa5e3bfb2eb1feabce20e
GeonwooVincentKim <kdsnop@gmail.com>
2021-07-06 12:56:59 +0900

    [Feature] - Print SmartPhoneGame instance information

--- src/Thread/ThreadExample.java
+++ src/Thread/ThreadExample.java
@@ -4,6 +4,85 @@ import static java.lang.System.out;
 
 public class ThreadExample {
     public static void main(String[] args) {
-        out.println();
+        SmartPhoneGame game = new SmartPhoneGame();
+        out.println(game);
+    }
+
+}
+
+class SmartPhoneGame {
+    private int level;
+
+    public int getLevel() {
+        return this.level;
+    }
+
+    // public void setLevel(int level) {
+    // this.level = level;
+    // }
+
+    public void increaseLevel() {
+        while (true) {
+            this.level++;
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+
+            } catch (Exception e) {
+                out.println(e.getMessage());
+                out.println("-------------------------");
+                e.printStackTrace();
+            }
+
+            out.println(Thread.currentThread().getName() + " Level : " + this.level);
+
+            if (this.level % 10 == 0)
+                break;
+        }
+    }
+}
+
+class Player1 extends Thread {
+    private SmartPhoneGame game;
+
+    public SmartPhoneGame getGame() {
+        return game;
+    }
+
+    public void setGame(SmartPhoneGame game) {
+        this.game = game;
+    }
+
+    public void setSmartPhoneGame(SmartPhoneGame game) {
+        this.setName("Player1");
+        this.setGame(game);
+    }
+
+    @Override
+    public void run() {
+        game.increaseLevel();
+    }
+}
+
+class Player2 extends Thread {
+    private SmartPhoneGame game;
+
+    public SmartPhoneGame getGame() {
+        return game;
+    }
+
+    public void setGame(SmartPhoneGame game) {
+        this.game = game;
+    }
+
+    public void setSmartPhoneGame(SmartPhoneGame game) {
+        this.setName("Player2");
+        this.setGame(game);
+    }
+
+    @Override
+    public void run() {
+        game.increaseLevel();
     }
 }
