baf957fb8956fb4198581e0397bda109e8db8a03
GeonwooVincentKim <kdsnop@gmail.com>
2021-07-07 10:23:47 +0900

    [Feature] - Execute the Method

--- src/Class/Interface/Annotation/AnnotationExample.java
+++ src/Class/Interface/Annotation/AnnotationExample.java
@@ -8,12 +8,21 @@ public class AnnotationExample {
     public static void main(String[] args) throws Exception {
         Method method[] = UserClass.class.getDeclaredMethods();
         out.println(method);
+        out.println();
 
         for (int i = 0; i < method.length; i++) {
             String methodName = method[i].getName();
             out.println(methodName);
 
-            // UserAnnotation annotation = method[i].getAnnotation(UserAnnotation.class);
+            UserAnnotation annotation = method[i].getAnnotation(UserAnnotation.class);
+            out.println(annotation);
+
+            out.print(methodName + " 의 Annotation");
+            out.print("value : " + annotation.value() + " ");
+            out.print("number : " + annotation.number() + " ");
+            out.println("\n");
+
+            method[i].invoke(new UserClass(), null); // Execute the Method
         }
     }
 }
