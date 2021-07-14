ab4aeaabae36ea9e49f5934f918ae9ad3499ce4a
GeonwooVincentKim <kdsnop@gmail.com>
2021-07-14 10:33:31 +0900

    [Feature] - Created Person.java file

---
+++ src/Class/ClassConstructor/Person.java
@@ -0,0 +1,53 @@
+package Class.ClassConstructor;
+
+public class Person {
+    private String name;
+    private String gender;
+    private int age;
+    private int height;
+    private int weight;
+
+    public Person() {
+
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
