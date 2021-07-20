e142aa942ad2d4e90ab50cfeb536dbd8831e40d2
GeonwooVincentKim <kdsnop@gmail.com>
2021-07-20 13:30:22 +0900

    [Feature] - Print the blank line

--- src/Collections/Object/EqualsExample.java
+++ src/Collections/Object/EqualsExample.java
@@ -10,22 +10,32 @@ public class EqualsExample {
         Obj obj2 = new Obj(100);
         out.println(obj2);
 
+        out.println("------------------------------------");
+
         if (obj1.equals(obj2)) {
             out.println("obj1 object and obj2 object are same");
         } else {
             out.println("obj1 object and obj2 object are different");
         }
 
+        out.println();
         out.println("====================================");
+        out.println();
 
         Obj obj3 = obj1;
         out.println(obj3);
+
+        out.println("------------------------------------");
 
         if (obj1.equals(obj3)) {
             out.println("obj1 Object and obj3 Object are same");
         } else {
             out.println("obj1 Object and obj3 Object are different");
         }
+
+        out.println();
+        out.println("====================================");
+        out.println();
 
         ObjOverride objOverride1 = new ObjOverride(100);
         out.println(objOverride1);
