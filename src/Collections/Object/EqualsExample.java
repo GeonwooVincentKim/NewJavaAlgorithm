7c644be7bc33c9c31703212b8ad1d328d7056710
GeonwooVincentKim <kdsnop@gmail.com>
2021-07-20 13:19:56 +0900

    [Feature] - Get the `obj1` object-address value

--- src/Collections/Object/EqualsExample.java
+++ src/Collections/Object/EqualsExample.java
@@ -6,5 +6,23 @@ public class EqualsExample {
     public static void main(String[] args) {
         Obj obj1 = new Obj(100);
         out.println(obj1);
+
+        Obj obj2 = new Obj(100);
+        out.println(obj2);
+
+        if (obj1.equals(obj2)) {
+            out.println("obj1 object and obj2 object are same");
+        } else {
+            out.println("obj1 object and obj2 object are different");
+        }
+
+        Obj obj3 = obj1;
+        out.println(obj3);
+
+        if (obj1.equals(obj3)) {
+            out.println("obj1 Object and obj3 Object are same");
+        } else {
+            out.println("obj1 Object and obj3 Object are different");
+        }
     }
 }
---
+++ src/Collections/Object/ObjOverride.java
@@ -0,0 +1,5 @@
+package Collections.Object;
+
+public class ObjOverride {
+
+}
