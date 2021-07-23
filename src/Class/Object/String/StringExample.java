4c8bbeea1a9d4a569799bcc5486823641e42b282
GeonwooVincentKim <kdsnop@gmail.com>
2021-07-23 14:17:26 +0900

    [Feature] - Added Long Type

--- src/Class/Object/String/StringExample.java
+++ src/Class/Object/String/StringExample.java
@@ -6,12 +6,18 @@ public class StringExample {
     public static void main(String[] args) {
         String[] str = { "1", "2", "3", "4" };
 
-        int sum = getTotalNumber(str);
+        // Print String array type index as Integer
+        int sum = getTotalIntegerNumber(str);
         String printResult = printTotalNumber(sum);
         out.println(printResult);
+
+        // Print String array type index as Long
+        Long sumLongType = getTotalLongNumber(str);
+        String printLongResult = printTotalNumber(sumLongType);
+        out.println(printLongResult);
     }
 
-    public static int getTotalNumber(String[] str) {
+    public static int getTotalIntegerNumber(String[] str) {
         int sum = 0;
 
         for (int i = 0; i < str.length; i++) {
@@ -23,7 +29,27 @@ public class StringExample {
         return sum;
     }
 
+    public static Long getTotalLongNumber(String[] str) {
+        Long sum = 0L;
+
+        for (int i = 0; i < str.length; i++) {
+            sum += Long.parseLong(str[i]);
+        }
+
+        return sum;
+    }
+
     public static String printTotalNumber(int sum) {
+        String result = "";
+
+        for (int i = 0; i < sum; i++) {
+            result += sum + "\n";
+        }
+
+        return result;
+    }
+
+    public static String printTotalNumber(Long sum) {
         String result = "";
 
         for (int i = 0; i < sum; i++) {
