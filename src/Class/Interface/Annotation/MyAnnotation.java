86107487b3e4bc9a78a3798dd84924e808afa185
GeonwooVincentKim <kdsnop@gmail.com>
2021-07-27 15:34:18 +0900

    [Feature] - Set the annotation that can be created when the Developer creates new `METHOD`

--- src/Class/Interface/Annotation/MyAnnotation.java
+++ src/Class/Interface/Annotation/MyAnnotation.java
@@ -1,9 +1,12 @@
 package Class.Interface.Annotation;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.METHOD }) // Use this Annotation when the developers creates new `METHOD` in the class
 public @interface MyAnnotation {
     String value() default "-";
 
