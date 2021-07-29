ea74a3ec1822716fd9003302532df9e829616617
GeonwooVincentKim <kdsnop@gmail.com>
2021-07-29 13:30:09 +0900

    [Feature] - Print Enumerate values

--- src/Collections/Iterator/Enummeration/EnummerationExample.java
+++ src/Collections/Iterator/Enummeration/EnummerationExample.java
@@ -7,6 +7,7 @@ import java.util.Iterator;
 import java.util.Vector;
 
 public class EnummerationExample {
+    // Print Vector values to get vector type values
     public static Vector<Integer> getIntegerValues(int vectorLength) {
         Vector<Integer> vector = new Vector<>();
 
@@ -17,6 +18,7 @@ public class EnummerationExample {
         return vector;
     }
 
+    // Print Enummerartion value to get enumerated value
     public static String printEnumeration(Enumeration<Integer> enumeration) {
         String result = "";
 
@@ -28,19 +30,24 @@ public class EnummerationExample {
     }
 
     public static void main(String[] args) {
+        // 1. Input the vectorLength to get Integer values by using Vector type values
         int vectorLength = 5;
         Vector<Integer> vector = getIntegerValues(vectorLength);
 
+        // 2. Enumerate Vector type value as Enumeration type values.
         Enumeration<Integer> enumeration = vector.elements();
         out.println(enumeration);
 
+        // 3. Print Enumerated values.
         out.println("Enumeration 출력 : ");
         String printEnumerationValue = printEnumeration(enumeration);
         out.println(printEnumerationValue);
 
+        // 4. Iterate vector type values.
         Iterator<Integer> it = vector.iterator();
         out.println(it);
 
+        // 5. Print Vector type values.
         out.println("Iterator 출력 : ");
         while (it.hasNext()) {
             out.println(it.next());
