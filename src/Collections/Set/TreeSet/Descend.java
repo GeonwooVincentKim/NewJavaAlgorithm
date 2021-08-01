0f3eae249ae6347d74520af5d6ebe8105bee21d1
GeonwooVincentKim <kdsnop@gmail.com>
2021-08-02 08:50:15 +0900

    [Feature] - Updated Descned.java file

--- src/Collections/Set/TreeSet/Descend.java
+++ src/Collections/Set/TreeSet/Descend.java
@@ -2,13 +2,18 @@ package Collections.Set.TreeSet;
 
 import java.util.Comparator;
 
+@SuppressWarnings("unchecked")
 public class Descend implements Comparator {
     @Override
     public int compare(Object o1, Object o2) {
-        Comparable c1 = (Comparable) o1;
-        Comparable c2 = (Comparable) o2;
+        Comparable<Object> c1 = (Comparable<Object>) o1;
+        Comparable<Object> c2 = (Comparable<Object>) o2;
 
         // Sort `Set` as Descending Order
-        return c1.compareTo(c2) * -1;
+        return functionFoo(c1, c2);
+    }
+
+    private int functionFoo(Comparable<Object> c1, Comparable<Object> c2) {
+        return c1.compareTo((Object) c2) * -1;
     }
 }
