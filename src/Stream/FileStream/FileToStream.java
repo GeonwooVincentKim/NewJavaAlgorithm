7244ffbeb72b9d134b5a8e913534afb14ea9bcc7
GeonwooVincentKim <kdsnop@gmail.com>
2021-08-02 16:27:41 +0900

    [Feature] - Added FileToStream.java file

---
+++ src/Stream/FileStream/FileToStream.java
@@ -0,0 +1,27 @@
+package Stream.FileStream;
+
+import static java.lang.System.out;
+
+public class FileToStream {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    out.println("t1 : " + i);
+
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        out.println(t1);
+        t1.start();
+    }
+}
